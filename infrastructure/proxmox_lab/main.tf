44899a804df5d5f1c637d87e4a7d7a0949962889
Miguel Alpañez <alpanez.alcalde@gmail.com>
2025-11-08 22:35:06 +0000

    Develop (#37)
    * fix: avoid invalid tag when workflow runs on schedule
    * fix: publish devcontainer images under owner namespace
    * refactor: slim devcontainers and enforce pre-commit (#36)
    * refactor: slim devcontainers and enforce pre-commit (#39)

--- infrastructure/proxmox_lab/main.tf
+++ infrastructure/proxmox_lab/main.tf
@@ -1,30 +1,10 @@
 terraform {
   required_version = "~> 1.7.0"
 
-  required_providers {
-    proxmox = {
-      source  = "Telmate/proxmox"
-      version = "~> 3.0"
-    }
-  }
+  required_providers {}
 }
 
-provider "proxmox" {
-  pm_api_url         = var.pm_api_url
-  pm_user            = var.pm_user
-  pm_token_id        = var.pm_token_id
-  pm_token_secret    = var.pm_token_secret
-  pm_tls_insecure    = var.pm_tls_insecure
-  pm_timeout         = var.pm_timeout
-  pm_log_enable      = var.pm_log_enable
-  pm_log_file        = var.pm_log_file
-  pm_log_levels      = var.pm_log_levels
-  pm_parallel        = var.pm_parallel
-  pm_task_timeout    = var.pm_task_timeout
-  pm_http_headers    = var.pm_http_headers
-  pm_api_token_ttl   = var.pm_api_token_ttl
-  pm_api_token_renew = var.pm_api_token_renew
-}
+provider "proxmox" {}
 
 locals {
   lab_vms = {
